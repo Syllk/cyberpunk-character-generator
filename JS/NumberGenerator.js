42e2b0f84ac7bc9a59f917744de3965c6440a1c8
Sylwia <SenPonadWszystko@gmail.com>
2019-07-11 20:27:20 +0200

    Added throwDiceWithSum method to NumberGenerator

--- JS/NumberGenerator.js
+++ JS/NumberGenerator.js
@@ -8,6 +8,17 @@ class NumberGenerator {
 		}
 		return results;
 	}
+
+	throwDiceWithSum(n,sides,modifier=0){
+		var sum = 0;
+		var throws = this.throwDice(n,sides);
+		for (var i = 0; i < throws.lenght; i++) {
+			sum += throws[i];
+		}
+		return sum + modifier;
+	}
 }
 window.numberGenerator = new NumberGenerator();
 console.log(numberGenerator.throwDice(5,10));
+console.log(numberGenerator.throwDiceWithSum(5,4));
+
