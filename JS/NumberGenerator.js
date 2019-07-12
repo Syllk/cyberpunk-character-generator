7764e240803d6be96914d07359cbd76dd1a5b333
Sylwia <SenPonadWszystko@gmail.com>
2019-07-12 14:24:10 +0200

    Added throwDiceWithLimit

--- JS/NumberGenerator.js
+++ JS/NumberGenerator.js
@@ -17,8 +17,21 @@ class NumberGenerator {
 		}
 		return sum + modifier;
 	}
+
+	throwDiceWithLimit(n,sides,minValue){
+		var results = []
+		for (var i = 0; i < n; i++) {
+			//var throwResult = Math.ceil(Math.random()*(sides-minValue+1)+minValue-1);
+			var throwResult = Math.floor(Math.random()*(sides-minValue+1)+minValue);
+			results.push(throwResult);
+
+		}
+		return results;
+
+	}
 }
 window.numberGenerator = new NumberGenerator();
 console.log(numberGenerator.throwDice(5,10));
 console.log(numberGenerator.throwDiceWithSum(5,4));
+console.log(numberGenerator.throwDiceWithLimit(5,8,2));
 
