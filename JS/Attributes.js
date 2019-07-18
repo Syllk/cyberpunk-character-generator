e27cb1eed825d2671f1f15b78698037ab7be25b4
Sylwia <SenPonadWszystko@gmail.com>
2019-07-18 20:36:23 +0200

    Added AttributesGenerator

--- JS/Attributes.js
+++ JS/Attributes.js
@@ -10,38 +10,5 @@ class Attributes {
     this.empathy = empathy;
     this.bodyBuilt = bodyBuilt;
 }
-throwDiceWithLimit(n, sides, minValue) {
-    var results = []
-    for (var i = 0; i < n; i++) {
-        var throwResult = Math.floor(Math.random() * (sides - minValue + 1) + minValue);
-        results.push(throwResult);
 
-    }
-    return results;
-}
-
-
-wyswietl() {
-    var throwResult = this.throwDiceWithLimit(9, 10, 2)
-    // console.log(throwResult);
-    console.log(this.inteligence + " " + throwResult[0]);
-    console.log(this.reflex + " " + throwResult[1]);
-    console.log(this.technology + " " + throwResult[2]);
-    console.log(this.composure + " " + throwResult[3]);
-    console.log(this.attractivness + " " + throwResult[4]);
-    console.log(this.luck + " " + throwResult[5]);
-    console.log(this.speed + " " + throwResult[6]);
-    console.log(this.empathy + " " + throwResult[7]);
-    console.log(this.bodyBuilt + " " + throwResult[8]);
-}
-throwSet() {
-    var throwSet = this.throwDiceWithLimit(9, 10, 2);
-    console.log(throwSet);
-}
-}
-
-var firstChara = new Attributes("Int", "Ref", "Tech", "Com", "Atr", "Lc", "Sp", "Emp", "BB");
-
-firstChara.wyswietl();
-firstChara.throwSet();
 
---
+++ JS/AttributesGenerator.js
@@ -0,0 +1,10 @@
+class AttributesGenerator {
+	generateAttributes(){
+		var numberSet = numberGenerator.throwDiceWithLimit(9,10,2);
+		var attributes = new Attributes (numberSet[0], numberSet[1], numberSet[2], numberSet[3], numberSet[4], numberSet[5], numberSet[6], numberSet[7], numberSet[8]);
+		return attributes;
+	}
+}
+
+window.attributesGenerator = new AttributesGenerator();
+
