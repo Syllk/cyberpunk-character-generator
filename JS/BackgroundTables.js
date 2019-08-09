3d5177df0e40f14c0e0b7bf15148c8986c22c543
Sylwia <SenPonadWszystko@gmail.com>
2019-08-09 17:00:28 +0200

    TableData.js changed BacgroundData.js

--- JS/BackgroundTables.js
+++ JS/BackgroundTables.js
@@ -25,30 +25,3 @@ var whatHappenedToParents = ["Twój rodzic(e) zginął na wojnie", "Twój rodzic
 "Twój rodzic(e) ma amnezję i cię nie pamięta", "Nigdy nie znałeś swojego rodzica(ów)", "Rodzic(e) ukrywają się, by cię chronić", "Zostałeś oddany krewnym na przechowanie", 
 "Wyrosłeś na ulicy i nigdy nie miałeś rodziców", "Rodzice oddali cię do adopcji", "Rodzice sprzedali cię dla pieniędzy"];
 
-class StyleGenerator{
-	genClothing(){
-	//var lengthClothing = clothing.length;
-	//console.log(lengthClothing);
-	return clothing[numberGenerator.throwDice(1, clothing.length)-1];
-	}
-	genHair(){
-	return hair[numberGenerator.throwDice(1, hair.length)-1];
-	}
-	genSymbol(){
-	return symbol[numberGenerator.throwDice(1, symbol.length)-1];	
-	}
-	genEthnicalBacground(){
-	return ethnicBackground[numberGenerator.throwDice(1, ethnicBackground.length)-1];
-	}
-	genParentsSocialStatus(){
-	return parentsSocialStatus[numberGenerator.throwDice(1, parentsSocialStatus.length)-1];
-	}
-	genParentsStatus(){
-	return parentsStatus[numberGenerator.throwDice(1, parentsStatus.length)-1];
-	}
-	genWhatHappenedToParents(){
-	return whatHappenedToParents[numberGenerator.throwDice(1, whatHappenedToParents.length)-1];
-	}
-}
-window.styleGenerator = new StyleGenerator();
-//console.log(genClothing());
